5e876b68b224efbd1aad2e3a40cd9ae770f151c2
akbarrajasa <akbarrajasa2@gmail.com>
2025-11-09 13:30:23 +0700

    Pembagian tugasnya kayak gini yaa

---
+++ BALIKINWOY.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+/* yash kita akan buat program nyatet barang yang dipinjem
+pembagian tugasnya perfitur, jadi kita totally akan buat 5 fitur
+1. input data peminjaman (Akbar Rajasa)
+2. nampilin semua data
+3. tandai barang yang udah dibalikin
+4. liat barang yang belum dibalikin
+5. itung total barang yang belum dibalikin
+6. cari barang
+karena mode ambis jadi 5+1 fitur kebanggaan :)
+*/
+
+return 0;
+
+}
