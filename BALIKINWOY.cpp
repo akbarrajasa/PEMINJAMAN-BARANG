573113a76f8a75616e343c23a816f10402e48442
aksaaa <akbarrajasa2@gmail.com>
2025-11-10 06:53:30 +0700

    Revisi Pembagian Tugas
    Updated feature assignments for the borrowing program.

--- BALIKINWOY.cpp
+++ BALIKINWOY.cpp
@@ -5,12 +5,12 @@ int main() {
 
 /* yash kita akan buat program nyatet barang yang dipinjem
 pembagian tugasnya perfitur, jadi kita totally akan buat 5 fitur
-1. input data peminjaman (Akbar Rajasa)
-2. nampilin semua data
-3. tandai barang yang udah dibalikin
-4. liat barang yang belum dibalikin
-5. itung total barang yang belum dibalikin
-6. cari barang
+1. input data peminjaman (Davina)
+2. nampilin semua data (Mahardika)
+3. tandai barang yang udah dibalikin (Nadia)
+4. liat barang yang belum dibalikin (Rima)
+5. itung total barang yang belum dibalikin (Akbar)
+6. cari barang (Akbar)
 karena mode ambis jadi 5+1 fitur kebanggaan :)
 */
 
