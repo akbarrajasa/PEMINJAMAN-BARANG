ade7c0dffb20f0b8be56a2784e66e3ff26b325c4
aksaaa <akbarrajasa2@gmail.com>
2025-11-10 22:54:38 +0700

    tambahan dikit
    Display borrowed item details for a specific month.

--- BALIKINWOY.cpp
+++ BALIKINWOY.cpp
@@ -53,6 +53,17 @@ cin>>tanggal[array];
  array++;
  
 }
+ cout<<"Data barang yang dipinjam pada bulan "<<bulan<<endl;
+
+for(int a=0; a<array; a++){
+    cout<<"Data Ke-"<<a+1<<endl;
+
+    cout<<"Nama Barang              = "<<barang[a]<<endl;
+    cout<<"Nama Peminjam            = "<<nama[a]<<endl;
+    cout<<"Status Peminjaman        = "<<status[a]<<endl;
+    cout<<"Tanggal pinjam           = "<<tanggal[a]<<endl;
+    cout<<endl;
+}
 return 0;
 
 }
