0f50998ac95e0460005f397858af2e977fb65f12
aksaaa <akbarrajasa2@gmail.com>
2025-11-10 22:49:54 +0700

    nampilin barang
    Add output for borrowed items in the specified month.

--- BALIKINWOY.cpp
+++ BALIKINWOY.cpp
@@ -14,6 +14,17 @@ pembagian tugasnya perfitur, jadi kita totally akan buat 5 fitur
 karena mode ambis jadi 5+1 fitur kebanggaan :)
 */
 
+  cout<<"Data barang yang dipinjam pada bulan "<<bulan<<endl;
+
+for(int a=0; a<array; a++){
+    cout<<"Data Ke-"<<a+1<<endl;
+
+    cout<<"Nama Barang              = "<<barang[a]<<endl;
+    cout<<"Nama Peminjam            = "<<nama[a]<<endl;
+    cout<<"Status Peminjaman        = "<<status[a]<<endl;
+    cout<<"Tanggal pinjam           = "<<tanggal[a]<<endl;
+    cout<<endl;
+}
 return 0;
 
 }
