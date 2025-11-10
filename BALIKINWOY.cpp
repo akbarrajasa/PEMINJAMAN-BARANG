c9b7d7c59f237bfd578b949af49463b90bb8c5e7
aksaaa <akbarrajasa2@gmail.com>
2025-11-10 22:52:28 +0700

    terbaru
    Removed main function and related comments.

--- BALIKINWOY.cpp
+++ BALIKINWOY.cpp
@@ -1,20 +1,4 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-
-/* yash kita akan buat program nyatet barang yang dipinjem
-pembagian tugasnya perfitur, jadi kita totally akan buat 5 fitur
-1. input data peminjaman (Davina)
-2. nampilin semua data (Mahardika)
-3. tandai barang yang udah dibalikin (Nadia)
-4. liat barang yang belum dibalikin (Rima)
-5. itung total barang yang belum dibalikin (Akbar)
-6. cari barang (Akbar)
-karena mode ambis jadi 5+1 fitur kebanggaan :)
-*/
-
-  cout<<"Data barang yang dipinjam pada bulan "<<bulan<<endl;
+cout<<"Data barang yang dipinjam pada bulan "<<bulan<<endl;
 
 for(int a=0; a<array; a++){
     cout<<"Data Ke-"<<a+1<<endl;
@@ -24,7 +8,4 @@ for(int a=0; a<array; a++){
     cout<<"Status Peminjaman        = "<<status[a]<<endl;
     cout<<"Tanggal pinjam           = "<<tanggal[a]<<endl;
     cout<<endl;
-}
-return 0;
-
 }
